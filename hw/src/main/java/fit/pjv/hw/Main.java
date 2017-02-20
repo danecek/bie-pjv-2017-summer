b3cc4e58f076fe3f2467a4cdc04658632a1e752f
Jiri Danecek <danecek@fit.cvut.cz>
2017-02-20 15:55:06 +0100

    Merge origin/master
    Conflicts:
    	richclient/pom.xml

---
+++ hw/src/main/java/fit/pjv/hw/Main.java
@@ -0,0 +1,14 @@
+package fit.pjv.hw;
+
+public class Main {
+
+    static void hello(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        //System.out.println("HW");
+        hello("Peter");
+    }
+
+}
